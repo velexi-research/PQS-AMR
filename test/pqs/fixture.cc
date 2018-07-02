5f577da905779557b33596a76e88981ee42f0078
Kevin Chu <kevin@velexi.com>
2018-07-01 17:41:23 -0700

    Fix license clause.

--- test/pqs/fixture.cc
+++ test/pqs/fixture.cc
@@ -6,9 +6,9 @@
 
 /*
  * ---------------------------------------------------------------------
- * COPYRIGHT/LICENSE. This file is part of the FDK package. It is
+ * COPYRIGHT/LICENSE. This file is part of the XYZ package. It is
  * subject to the license terms in the LICENSE file found in the
- * top-level directory of this distribution. No part of the FDK
+ * top-level directory of this distribution. No part of the XYZ
  * package, including this file, may be copied, modified, propagated,
  * or distributed except according to the terms contained in the
  * LICENSE file.
