a4f8c82c15f966fdf9326a216cee1832fdb11145
Kevin Chu <kevin@velexi.com>
2018-07-03 08:57:42 -0700

    Add TestInterfaceInitModule.h

---
+++ test/pqs/TestInterfaceInitModule.h
@@ -0,0 +1,51 @@
+/*! \file TestInterfaceInitModule.h
+ *
+ * \brief
+ * Concrete implementation of pqs::InterfaceInitStrategy to use for testing.
+ */
+
+/*
+ * ---------------------------------------------------------------------
+ * COPYRIGHT/LICENSE. This file is part of the XYZ package. It is
+ * subject to the license terms in the LICENSE file found in the
+ * top-level directory of this distribution. No part of the XYZ
+ * package, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the
+ * LICENSE file.
+ * ---------------------------------------------------------------------
+ */
+
+// --- Headers, namespaces, and type declarations
+
+// Boost
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+// SAMRAI
+#include "SAMRAI/SAMRAI_config.h"  // IWYU pragma: keep
+
+// PQS
+#include "PQS/PQS_config.h"  // IWYU pragma: keep
+#include "PQS/pqs/InterfaceInitStrategy.h"
+
+// Namespaces
+using namespace std;
+using namespace SAMRAI;
+using namespace PQS;
+
+// Class/type declarations
+
+
+// --- Fixtures
+
+namespace pqsTests {
+
+class TestInterfaceInitModule: public pqs::InterfaceInitStrategy
+{
+public:
+    /*
+     * No-op. Only used for testing.
+     */
+    virtual void initializeInterface(hier::Patch& patch, int phi_id) {};
+};
+
+} // pqsTests namespace
