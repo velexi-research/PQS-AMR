d7de51349558ac61e37b8197a3b524a6fd3a6f6e
Kevin Chu <kevin@velexi.com>
2018-08-21 21:59:12 -0700

    Add code for initializing fluid-fluid interface.
    - Remove usage of Boost library.

--- examples/no-solid-phase/InterfaceInitModule.cc
+++ examples/no-solid-phase/InterfaceInitModule.cc
@@ -1,8 +1,8 @@
 /*! \file InterfaceInitModule.cc
  *
  * \brief
- * Concrete implementation of pqs::InterfaceInitStrategy to use for TODO
- * example.
+ * Implementation file for concrete subclass of pqs::InterfaceInitStrategy to
+ * use in example application that has no solid phase.
  */
 
 /*
@@ -18,20 +18,29 @@
 
 // --- Headers, namespaces, and type declarations
 
-// Boost
-#include <boost/smart_ptr/shared_ptr.hpp>
+// Standard library
+#include <stddef.h>
+#include <string>
 
 // SAMRAI
 #include "SAMRAI/SAMRAI_config.h"  // IWYU pragma: keep
+#include "SAMRAI/geom/CartesianPatchGeometry.h"
+#include "SAMRAI/hier/Box.h"
+#include "SAMRAI/hier/IntVector.h"
 #include "SAMRAI/hier/Patch.h"
 #include "SAMRAI/pdat/CellData.h"
+#include "SAMRAI/tbox/Database.h"
 #include "SAMRAI/tbox/Utilities.h"
 
 // PQS
 #include "PQS/PQS_config.h"  // IWYU pragma: keep
+#include "PQS/utilities/error.h"
+#include "PQS/utilities/macros.h"
 
-// PQS test
+// PQS application
 #include "InterfaceInitModule.h"
+#include "kernels/interface_kernels_2d.h"
+#include "kernels/interface_kernels_3d.h"
 
 // Namespaces
 using namespace std;
@@ -39,17 +48,165 @@ using namespace SAMRAI;
 using namespace PQS;
 
 // Class/type declarations
-namespace SAMRAI { namespace hier { class PatchData; } }
 
 
-// --- Class implementation
+// --- Public Methods
+
+InterfaceInitModule::InterfaceInitModule(
+        const shared_ptr<tbox::Database>& config_db, const int dim)
+{
+    // Check arguments
+    if (config_db == NULL) {
+        PQS_ERROR(this, "InterfaceInitModule", "'config_db' must not be NULL");
+    }
+    verifyConfigurationDatabase(config_db);
+
+    if ((dim != 2) and (dim != 3)) {
+        PQS_ERROR(this, "InterfaceInitModule",
+                  "'num_dimensions' must equal 2 or 3");
+    }
+
+    // Get problem dimension
+    d_dim = dim;
+    d_center = new double[dim];
+
+    // Load configuration from config_db
+    loadConfiguration(config_db);
+} // IntefaceInitModule::IntefaceInitModule()
+
+InterfaceInitModule::~InterfaceInitModule()
+{
+    delete [] d_center;
+} // IntefaceInitModule::~IntefaceInitModule()
 
 void InterfaceInitModule::initializeInterface(
         hier::Patch& patch, int phi_id)
 {
-    boost::shared_ptr< pdat::CellData<PQS_REAL> > phi_data =
-            BOOST_CAST<pdat::CellData<PQS_REAL>, hier::PatchData>(
+    // --- Preparations
+
+    // Get geometry parameters
+    shared_ptr<geom::CartesianPatchGeometry> patch_geom =
+            SAMRAI_SHARED_PTR_CAST<geom::CartesianPatchGeometry>(
+                    patch.getPatchGeometry());
+    const double* dx = patch_geom->getDx();
+    const double* x_lower = patch_geom->getXLower();
+
+    // ------- Get pointers to data and index space ranges
+
+    // phi
+    shared_ptr< pdat::CellData<PQS_REAL> > phi_data =
+            SAMRAI_SHARED_PTR_CAST< pdat::CellData<PQS_REAL> >(
                     patch.getPatchData(phi_id));
 
-    phi_data->fill(1.0);
-}
+    hier::Box phi_ghostbox = phi_data->getGhostBox();
+    const hier::IntVector phi_ghostbox_lower = phi_ghostbox.lower();
+    const hier::IntVector phi_ghostbox_upper = phi_ghostbox.upper();
+    PQS_INT_VECT_TO_INT_ARRAY(phi_ghostbox_lo, phi_ghostbox_lower);
+    PQS_INT_VECT_TO_INT_ARRAY(phi_ghostbox_hi, phi_ghostbox_upper);
+
+    PQS_REAL* phi = phi_data->getPointer();
+
+    // fill box
+    hier::Box fillbox = patch.getBox();
+    const hier::IntVector fillbox_lower = fillbox.lower();
+    const hier::IntVector fillbox_upper = fillbox.upper();
+    PQS_INT_VECT_TO_INT_ARRAY(fillbox_lo, fillbox_lower);
+    PQS_INT_VECT_TO_INT_ARRAY(fillbox_hi, fillbox_upper);
+
+    // --- Initialize phi
+
+    if (d_dim == 2) {
+        if (d_interface_type == "circle") {
+            INIT_CIRCLE(phi,
+                        phi_ghostbox_lo, phi_ghostbox_hi,
+                        fillbox_lo, fillbox_hi,
+                        x_lower,
+                        dx,
+                        d_center,
+                        &d_radius);
+        } else if (d_interface_type == "starfish") {
+            INIT_STARFISH(phi,
+                          phi_ghostbox_lo, phi_ghostbox_hi,
+                          fillbox_lo, fillbox_hi,
+                          x_lower,
+                          dx,
+                          d_center,
+                          &d_radius);
+        } else {
+            PQS_ERROR(this, "initializeInterface",
+                      string("interface_type=") + d_interface_type +
+                      string(". Valid values: circle, starfish"));
+        }
+    } else if (d_dim == 3) {
+        if (d_interface_type == "sphere") {
+            INIT_SPHERE(phi,
+                        phi_ghostbox_lo, phi_ghostbox_hi,
+                        fillbox_lo, fillbox_hi,
+                        x_lower,
+                        dx,
+                        d_center,
+                        &d_radius);
+        } else if (d_interface_type == "bumpy sphere") {
+            INIT_BUMPY_SPHERE(phi,
+                          phi_ghostbox_lo, phi_ghostbox_hi,
+                          fillbox_lo, fillbox_hi,
+                          x_lower,
+                          dx,
+                          d_center,
+                          &d_radius);
+        } else {
+            PQS_ERROR(this, "initializeInterface",
+                      string("interface_type=") + d_interface_type +
+                      string(". Valid values: circle, starfish"));
+        }
+    }
+} // InterfaceInitModule::initializeInterface()
+
+
+// --- Private methods
+
+void InterfaceInitModule::verifyConfigurationDatabase(
+    const shared_ptr<tbox::Database>& config_db) const
+{
+    // --- Check arguments
+
+    if (config_db == NULL) {
+        PQS_ERROR(this, "verifyConfigurationDatabase",
+                  "'config_db' must not be NULL");
+    }
+
+    // --- Verify configuration parameters
+
+    if (!config_db->isString("interface_type")) {
+        PQS_ERROR(this, "verifyConfigurationDatabase",
+                  "'interface_type' missing from configuration database");
+    }
+
+    if (!config_db->keyExists("center")) {
+        PQS_ERROR(this, "verifyConfigurationDatabase",
+                  "'center' missing from configuration database");
+    }
+    if (!config_db->isDouble("radius")) {
+        PQS_ERROR(this, "verifyConfigurationDatabase",
+                  "'radius' missing from 'PQS' database");
+    }
+
+} // InterfaceInitModule::verifyConfigurationDatabase()
+
+void InterfaceInitModule::loadConfiguration(
+        const shared_ptr<tbox::Database>& config_db)
+{
+    // --- Check arguments
+
+    if (config_db == NULL) {
+        PQS_ERROR(this, "loadConfiguration",
+                  "'config_db' must not be NULL");
+    }
+
+    // --- Load configuration parameters
+
+    d_interface_type = config_db->getString("interface_type");
+    config_db->getDoubleArray("center", d_center, d_dim);
+    d_radius = config_db->getDouble("radius");
+
+} // InterfaceInitModule::loadConfiguration()
--- examples/no-solid-phase/InterfaceInitModule.h
+++ examples/no-solid-phase/InterfaceInitModule.h
@@ -1,8 +1,8 @@
 /*! \file InterfaceInitModule.h
  *
  * \brief
- * Concrete implementation of pqs::InterfaceInitStrategy to use for TODO
- * example.
+ * Header file for concrete subclass of pqs::InterfaceInitStrategy to use in
+ * example application that has no solid phase.
  */
 
 /*
@@ -18,6 +18,10 @@
 
 // --- Headers, namespaces, and type declarations
 
+// Standard library
+#include <iosfwd>
+#include <memory>
+
 // SAMRAI
 #include "SAMRAI/SAMRAI_config.h"  // IWYU pragma: keep
 
@@ -32,6 +36,7 @@ using namespace PQS;
 
 // Class/type declarations
 namespace SAMRAI { namespace hier { class Patch; } }
+namespace SAMRAI { namespace tbox { class Database; } }
 
 
 // --- Class declaration
@@ -39,8 +44,97 @@ namespace SAMRAI { namespace hier { class Patch; } }
 class InterfaceInitModule: public pqs::InterfaceInitStrategy
 {
 public:
+
+    //! @{
+
+    /*!
+     ************************************************************************
+     *
+     * @name Constructor and destructor
+     *
+     ************************************************************************/
+
+    /*!
+     * The constructor loads configuration parameters and allocates
+     * memory required by the object.
+     *
+     * Parameters
+     * ----------
+     * config_db: database containing configuration parameters
+     *
+     * dim: problem dimension
+     */
+    InterfaceInitModule(
+            const shared_ptr<tbox::Database>& config_db,
+            const int dim);
+
+    /*!
+     * Default destructor frees memory allocated for the object.
+     */
+    virtual ~InterfaceInitModule();
+
+    //! @}
+
+    //! {@
+
+    /*!
+     ************************************************************************
+     *
+     * @name Interface initialization methods
+     *
+     ************************************************************************/
+
     /*
-     * Set value at all grid points to 1.0.
+     * Set phi so that fluid-fluid interface is a perturbed circle/sphere
+     * that is fully contained in computational domain.
      */
     virtual void initializeInterface(hier::Patch& patch, int phi_id);
+
+    //! @}
+
+protected:
+
+    /****************************************************************
+     *
+     * Data Members
+     *
+     ****************************************************************/
+
+    // --- Parameters
+
+    int d_dim;  // problem dimension
+
+    string d_interface_type;
+    double *d_center;
+    double d_radius;
+
+private:
+
+    /*
+     * Verify that configuration database from specified database.
+     *
+     * Parameters
+     * ----------
+     * config_db: database containing configuration parameters
+     *
+     * Return value
+     * ------------
+     * true
+     *
+     * Notes
+     * -----
+     * - If 'config_db' is not valid, this method throws an exception
+     *   containing error information.
+     */
+     void verifyConfigurationDatabase(
+        const shared_ptr<tbox::Database>& config_db) const;
+
+    /*
+     * Load configuration parameters from specified database.
+     *
+     * Parameters
+     * ----------
+     * config_db: database containing configuration parameters
+     */
+    void loadConfiguration(const shared_ptr<tbox::Database>& config_db);
 };
