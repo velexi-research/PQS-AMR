05abacdc31a653f0186712c3683a48bf4cddb5ed
Kevin Chu <kevin@velexi.com>
2018-09-05 20:39:09 -0700

    Add unit tests for area when phi > 0.

--- test/lsm/test_lsm_2d.cc
+++ test/lsm/test_lsm_2d.cc
@@ -44,8 +44,8 @@ using namespace std;
 
 namespace lsmTests {
 
-// Test case: compute area (no control volume)
-TEST_F(lsmTest, test_compute_area)
+// Test case: compute area phi < 0 (no control volume)
+TEST_F(lsmTest, test_compute_area_phi_lt_zero)
 {
     // --- Preparations
 
@@ -111,6 +111,82 @@ TEST_F(lsmTest, test_compute_area)
     area = PQS_LSM_2D_AREA_PHI_LESS_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
                                               ib_lo, ib_hi,
                                               dx, &eps);
+    EXPECT_NEAR((area - expected_area)/expected_area, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // --- Clean up
+    //
+}
+
+// Test case: compute area phi > 0 (no control volume)
+TEST_F(lsmTest, test_compute_area_phi_gt_zero)
+{
+    // --- Preparations
+
+    num_dimensions = 2;
+
+    // --- Exercise functionality and check results
+
+    // ------ circles
+
+    double radius;
+    double area, expected_area;
+    double eps;
+
+    // radius = 0.25, N = 200
+    N = 200;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.25;
+    expected_area = 4 - M_PI * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_CIRCLE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    area = PQS_LSM_2D_AREA_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
+    EXPECT_NEAR((area - expected_area)/expected_area, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // radius = 0.5, N = 100
+    N = 100;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.5;
+    expected_area = 4 - M_PI * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_CIRCLE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    area = PQS_LSM_2D_AREA_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
+    EXPECT_NEAR((area - expected_area)/expected_area, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // radius = 0.75, N = 500
+    N = 500;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.75;
+    expected_area = 4 - M_PI * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_CIRCLE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    area = PQS_LSM_2D_AREA_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
     EXPECT_NEAR((area - expected_area)/expected_area, 0, eps);
 
     lsmTest::tearDownGridFunction();
