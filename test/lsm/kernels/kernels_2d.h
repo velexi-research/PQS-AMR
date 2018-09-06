8d74b1feebe5b1156e90e5c1b4cb91c293c57372
Kevin Chu <kevin@velexi.com>
2018-09-05 21:06:11 -0700

    Remove extraneous function declaration in unit tests.

--- test/lsm/kernels/kernels_2d.h
+++ test/lsm/kernels/kernels_2d.h
@@ -61,39 +61,6 @@ void SET_PHI_CIRCLE(
     const double *dx,
     const double *r);
 
-/*!
- *
- * Compute area of region where signed distance function is greater than 0.
- *
- * Parameters
- * ----------
- * phi: signed distance function
- *
- * dx: array containing grid spacing in each coordinate direction
- *
- * eps: width of numerical smoothing to use for Heaviside function
- *
- * *_gb_lo: lower corner of index range for ghostbox
- *
- * *_gb_hi: upper corner of index range for ghostbox
- *
- * ib_lo: lower corner of index range for interior box
- *
- * ib_hi: upper corner of index range for interior box
- *
- * Return value
- * ------------
- * area of region where phi > 0
- */
-PQS_REAL PQS_LSM_2D_AREA_PHI_GREATER_THAN_ZERO(
-    const PQS_REAL* phi,
-    const int *phi_gb_lo,
-    const int *phi_gb_hi,
-    const int *ib_lo,
-    const int * ib_hi,
-    const double *dx,
-    const double *eps);
-
 #ifdef __cplusplus
 }
 #endif
