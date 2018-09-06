b5143418a28489006be46c1bc0f6010125283f8b
Kevin Chu <kevin@velexi.com>
2018-09-05 21:09:41 -0700

    Add 3D LSM unit tests.

---
+++ test/lsm/kernels/kernels_3d.h
@@ -0,0 +1,66 @@
+/*! \file kernels_3d.h
+ *
+ * \brief
+ * Header file numerical kernels for Level Set Method computations in
+ * three space dimensions
+ */
+
+/*
+ * ---------------------------------------------------------------------
+ * COPYRIGHT/LICENSE.  This file is part of the XYZ package.  It is
+ * subject to the license terms in the LICENSE file found in the
+ * top-level directory of this distribution.  No part of the XYZ
+ * package, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the
+ * LICENSE file.
+ * ---------------------------------------------------------------------
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Link between C/C++ and Fortran function names
+ *
+ *      name in                          name in
+ *      C/C++ code                       Fortran code
+ *      ----------                       ------------
+ */
+#define SET_PHI_SPHERE                   setphisphere_
+
+/*!
+ * Compute signed distance function for sphere of specified radius
+ * centered at the origin.
+ *
+ * Parameters
+ * ----------
+ * phi: [output] signed distance function
+ *
+ * x_lo: array containing lower corner of grid
+ *
+ * dx: array containing grid spacing in each coordinate direction
+ *
+ * r: radius of sphere
+ *
+ * *_gb_lo: lower corner of index range for ghostbox
+ *
+ * *_gb_hi: upper corner of index range for ghostbox
+ *
+ * ib_lo: lower corner of index range for interior box
+ *
+ * ib_hi: upper corner of index range for interior box
+ *
+ */
+void SET_PHI_SPHERE(
+    const PQS_REAL* phi,
+    const int *phi_gb_lo,
+    const int *phi_gb_hi,
+    const int *ib_lo,
+    const int *ib_hi,
+    const double *x_lo,
+    const double *dx,
+    const double *r);
+
+#ifdef __cplusplus
+}
+#endif
--- test/lsm/test_lsm_3d.cc
+++ test/lsm/test_lsm_3d.cc
@@ -18,9 +18,12 @@
 // --- Headers, namespaces, and type declarations
 
 // Standard library
+#include <cmath>
 
 // Google Test
 #include "gtest/gtest.h"
+#include "gtest/gtest-message.h"
+#include "gtest/gtest-test-part.h"
 
 // PQS
 #include "PQS/PQS_config.h"  // IWYU pragma: keep
@@ -28,6 +31,8 @@
 
 // PQS test
 #include "fixture.h"
+#include "kernels/kernels_3d.h"
+
 
 // Namespaces
 using namespace std;
@@ -39,24 +44,156 @@ using namespace std;
 
 namespace lsmTests {
 
-// Test case: compute volume (no control volume)
-TEST_F(lsmTest, test_compute_volume)
+// Test case: compute volume phi < 0 (no control volume)
+TEST_F(lsmTest, test_compute_volume_phi_lt_zero)
 {
     // --- Preparations
 
-    // Configure and initialize geometry and PatchHierarchy
     num_dimensions = 3;
-    N = 10;
+
+    // --- Exercise functionality and check results
+
+    // ------ circles
+
+    double radius;
+    double volume, expected_volume;
+    double eps;
+
+    // radius = 0.25, N = 200
+    N = 200;
     lsmTest::setUpGrid();
     lsmTest::setUpGridFunction();
 
-    // --- Exercise functionality
+    radius = 0.25;
+    expected_volume = 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_LESS_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                              ib_lo, ib_hi,
+                                              dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
 
-    // TODO
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
 
-    // --- Check results
+    // radius = 0.5, N = 100
+    N = 100;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
 
-    // TODO
+    radius = 0.5;
+    expected_volume = 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_LESS_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                              ib_lo, ib_hi,
+                                              dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // radius = 0.75, N = 250
+    N = 250;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.75;
+    expected_volume = 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_LESS_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                              ib_lo, ib_hi,
+                                              dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // --- Clean up
+    //
+}
+
+// Test case: compute volume phi > 0 (no control volume)
+TEST_F(lsmTest, test_compute_volume_phi_gt_zero)
+{
+    // --- Preparations
+
+    num_dimensions = 3;
+
+    // --- Exercise functionality and check results
+
+    // ------ circles
+
+    double radius;
+    double volume, expected_volume;
+    double eps;
+
+    // radius = 0.25, N = 200
+    N = 200;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.25;
+    expected_volume = 8 - 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // radius = 0.5, N = 100
+    N = 100;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.5;
+    expected_volume = 8 - 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // radius = 0.75, N = 250
+    N = 250;
+    lsmTest::setUpGrid();
+    lsmTest::setUpGridFunction();
+
+    radius = 0.75;
+    expected_volume = 8 - 4 * M_PI / 3 * radius * radius * radius;
+    eps = 1.5 * dx[0];
+    SET_PHI_SPHERE(phi, phi_gb_lo, phi_gb_hi,
+                   ib_lo, ib_hi,
+                   x_lo, dx, &radius);
+    volume = LSM_3D_VOLUME_PHI_GREATER_THAN_ZERO(phi, phi_gb_lo, phi_gb_hi,
+                                                 ib_lo, ib_hi,
+                                                 dx, &eps);
+    EXPECT_NEAR((volume - expected_volume)/expected_volume, 0, eps);
+
+    lsmTest::tearDownGridFunction();
+    lsmTest::tearDownGrid();
+
+    // --- Clean up
+    //
 }
 
 } // lsmTests namespace
